2731cbcdeba15f1e0dc0536d9c2fbca86f4d020f
Shashwat Singh <singhsha345@gmail.com>
2025-05-31 17:18:08 +0530

    update navabar section

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("fa-x");
   list.classList.toggle("nav-list-active");
 });
+
+//cart
+const cartCount = document.getElementById("cart-count");
+const addToCartButtons = document.querySelectorAll(".add-to-cart-btn");
+
+let count = 0;
+
+addToCartButtons.forEach(btn => {
+  btn.addEventListener("click", () => {
+    count++;
+    cartCount.textContent = count;
+  });
+});
